25cc3670fa9ac03c59709a9298160dd64522f613
Steph Monette <steph@muledesign.com>
2016-01-05 08:13:47 -0800

    adding more pseduo code

--- game.js
+++ game.js
@@ -5,14 +5,17 @@
 
 // PLACEMENT
 
-// Each player has array with 5 ships, top point of each
-// coordinates of second section array -1
-// coordinates of third section - 2
+// Each player has array with 5 ships, top point of each. PUSH TO ARRAY
+// coordinates of second section array -1. PUSH TO ARRAY
+// coordinates of third section - 2 PUSH TO ARRAY
+
+// placement1 = [[ 0, 1, 2, 1, 0],[1, 2, 3, 2, 1],[2, 3, 4, 3, 2]];
+// placement2 = [[ 0, 1, 2, 1, 0],[1, 2, 3, 2, 1],[2, 3, 4, 3, 2]];
 
 // HIT
 
 // each strike has a coordinate
-// compare to coordinates in arrays
+// iterate over arrays to find matching coordinates
 
 
 // WINNING
