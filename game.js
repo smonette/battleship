76415dbe89ad767aa86dac97e9cfa760aca3a18c
Steph Monette <steph@muledesign.com>
2016-01-05 22:40:45 -0800

    crap logic for hits vs misses

--- game.js
+++ game.js
@@ -2,17 +2,10 @@
 
 
 window.onload = function() {
-
-	// PLACEMENT
 	var boxes = document.getElementsByClassName('grid-square');
-	for(var i = 0; i < boxes.length; i++) {
-		var box = boxes[i];
-		box.onclick = function(){
-			var coordX = this.getAttribute("data-x");
-			var coordY = this.getAttribute("data-y");
-			alert( "X is " + coordX + ", and Y is " + coordY);
-		}
-	}
+
+  // PLACEMENT
+
 	// Each player has array with 5 ships, top point of each. PUSH TO ARRAY
 	// coordinates of second section array -1. PUSH TO ARRAY
 	// coordinates of third section - 2 PUSH TO ARRAY
@@ -21,10 +14,28 @@ window.onload = function() {
 
 	// placement2 = [[ 0, 1, 2, 1, 0],[1, 2, 3, 2, 1],[2, 3, 4, 3, 2]];
 
+  var placement_sample = [00,01,02,11,12,13,32,33,34] 
 
 
 
-// HIT
+// SHOTS FIRED
+  for (var i = 0; i < boxes.length; i++) {
+    var shot = boxes[i];
+    shot.onclick = function(){
+      var coordX = this.getAttribute("data-x");
+      var coordY = this.getAttribute("data-y");
+      var coordShot = parseInt(coordX + coordY);
+
+        if( placement_sample.indexOf(coordShot) == -1) {      
+          this.classList.add('miss');
+          console.log("miss!" + coordShot);
+        } else {
+          this.classList.add('hit');
+          console.log("hit!" + coordShot);
+        }
+      console.log("X is " + coordX + ", and Y is " + coordY);
+    }
+  }
 // each strike has a coordinate
 // iterate over arrays to find matching coordinates
 
