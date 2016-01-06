9d7e583ecd60c181952af0c4c4629ca43d464eb2
Steph Monette <steph@muledesign.com>
2016-01-05 22:03:43 -0800

    getting coordinates when a user clicks on the grid

--- game.js
+++ game.js
@@ -1,24 +1,35 @@
 // place for all the logic controlling the game
 
 
+window.onload = function() {
+
+	// PLACEMENT
+	var boxes = document.getElementsByClassName('grid-square');
+	for(var i = 0; i < boxes.length; i++) {
+		var box = boxes[i];
+		box.onclick = function(){
+			var coordX = this.getAttribute("data-x");
+			var coordY = this.getAttribute("data-y");
+			alert( "X is " + coordX + ", and Y is " + coordY);
+		}
+	}
+	// Each player has array with 5 ships, top point of each. PUSH TO ARRAY
+	// coordinates of second section array -1. PUSH TO ARRAY
+	// coordinates of third section - 2 PUSH TO ARRAY
+
+	// placement1 = [[ 0, 1, 2, 1, 0],[1, 2, 3, 2, 1],[2, 3, 4, 3, 2]];
+
+	// placement2 = [[ 0, 1, 2, 1, 0],[1, 2, 3, 2, 1],[2, 3, 4, 3, 2]];
 
 
-// PLACEMENT
 
-// Each player has array with 5 ships, top point of each. PUSH TO ARRAY
-// coordinates of second section array -1. PUSH TO ARRAY
-// coordinates of third section - 2 PUSH TO ARRAY
-
-// placement1 = [[ 0, 1, 2, 1, 0],[1, 2, 3, 2, 1],[2, 3, 4, 3, 2]];
-
-// placement2 = [[ 0, 1, 2, 1, 0],[1, 2, 3, 2, 1],[2, 3, 4, 3, 2]];
 
 // HIT
-
 // each strike has a coordinate
 // iterate over arrays to find matching coordinates
 
 
 // WINNING
-
 // All coordinates are hit
+
+}
